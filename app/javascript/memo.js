0560751459b018940ec2e8ec1f38d28d74b11345
peso-02 <xwpswgzv7j@privaterelay.appleid.com>
2026-01-14 00:28:42 +0900

    リクエストを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,6 +1,6 @@
 function post (){
- const form = document.getElementById("form");
- form.addEventListener("submit", (e) => {
+  const form = document.getElementById("form");
+  form.addEventListener("submit", (e) => {
     e.preventDefault();
     const formData = new FormData(form);
     const XHR = new XMLHttpRequest();
